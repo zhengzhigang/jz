abe6c73011dc7ad62005c32f2b70e4dfe23ffb22
zhengzhigang <zhengzhigang@igengmei.com>
2019-01-02 16:15:18 +0800

    智慧教室修改

--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "project2/RJ-css"
-sass_dir = "project2-scss"
+css_dir = "project/RJ-css"
+sass_dir = "project-scss"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
