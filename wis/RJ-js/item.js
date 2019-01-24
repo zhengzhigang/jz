51bcd499ee84c06bf0bfe04f789161774daac734
zhengzhigang <zhengzhigang@igengmei.com>
2019-01-24 15:16:40 +0800

    wis

--- wis/RJ-js/item.js
+++ wis/RJ-js/item.js
@@ -9,7 +9,14 @@ $(function() {
         autoplayDisableOnInteraction: false
     });
 
+    var width = $(window).width()
     $('.sec8__content').on('click', '.sec8__circle', function() {
-        $(this).addClass('active').siblings('.sec8__circle').removeClass('active');
+        if (width > 768) {
+            $(this).addClass('active').siblings('.sec8__circle').removeClass('active');
+        }
+        if (width <= 768) {
+            $(this).addClass('active').siblings().addClass('active');
+            $(this).parents('item').find('.sec8__circle').removeClass('active').siblings().removeClass('active');
+        }
     })
 })
