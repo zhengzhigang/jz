5d04b468b10f778a5f1f99fa7fc62ede29e78058
zhengzhigang <zhengzhigang@igengmei.com>
2019-01-25 14:32:55 +0800

    wis

--- wis/RJ-js/item.js
+++ wis/RJ-js/item.js
@@ -3,23 +3,12 @@ $(function() {
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         centeredSlides: true,
-        autoplay: 2500,
+        autoplay: 10000,
         loop: true,
         autoplayDisableOnInteraction: false
     }
     new Swiper('.sec4__swiper .swiper-container', option);
     new Swiper('.sec11__swiper .swiper-container', option);
-
-    var width = $(window).width()
-    $('.sec8__content').on('click', '.sec8__circle', function() {
-        if (width > 768) {
-            $(this).addClass('active').siblings('.sec8__circle').removeClass('active');
-        }
-        if (width <= 768) {
-            $(this).addClass('active').siblings().addClass('active');
-            $(this).parents('item').find('.sec8__circle').removeClass('active').siblings().removeClass('active');
-        }
-    })
 
     $('.sec11__swiper .slide3 tr').hover(function() {
         $(this).siblings().removeClass('active');
