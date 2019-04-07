7cf6a52aa527ce6404358b45f0267a2985971e64
zhengzhigang <813726742@qq.com>
2019-04-07 22:34:47 +0800

    chengdu

--- chengdu/RJ-js/item.js
+++ chengdu/RJ-js/item.js
@@ -1,13 +1,24 @@
+/*
+ * file-name：item.js
+ * author：ZhengZhiGang
+ * 描述：页面js
+ * date：2019-04-07
+ */
 $(function(){
     var page = {
         firstScreenHeight: null,
+        headerHeight: 0,
         isClick: false,
         init: function() {
+            var winWidth = $(window).width();
             $('.sec3__btn').on('click', this.showQrCode);
             $('.qr__code').on('click', '.close', this.hideQrCode);
-            this.fixedNav();
-            this.clickNav();
-            window.onscroll()
+            this.checkLiveTime();
+            if (winWidth > 768) {
+                this.fixedNav();
+                this.clickNav();
+                window.onscroll()
+            }
         },
 
         showQrCode: function() {
@@ -18,17 +29,45 @@ $(function(){
             $('.qr__code').hide();
         },
 
+        checkLiveTime: function() {
+            var _this = this;
+            $.ajax({
+                url: 'http://www.baidu.com',
+                type: 'get',
+                dataType: 'json',
+                success: function(data) {
+                    if (data.Status) {
+                        // TODO 直播时间
+                        $('.sec3__btn').remove();
+                        $('.sec3__video').on('click', _this.loadLive);
+                    }
+                },
+                error: function(error) {
+                    console.log('error:', error.statusText)
+                }
+            })
+        },
+
+        loadLive: function (userData) {
+            $('.s-video').unbind('click');
+            var iframe = $('<iframe id="iframe" scrolling="no" style="width: 100%; height: 600px;" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"/>');
+            iframe.attr('src', 'http://live.vhall.com/webinar/inituser/990246840?k=' + new Date().getTime());
+            $('.sec3__video').empty();
+            $('.sec3__video').append(iframe);
+        },
+
         fixedNav: function() {
             var _this = this;
             // 导航吸顶
             window.onscroll = this.debounce(function() {
                 var wScrollTop = $(window).scrollTop()
                 if (_this.firstScreenHeight === null) {
+                    _this.headerHeight = $('.header').height() ? $('.header').height() : 0;
                     _this.firstScreenHeight = $('.sec1').height();
                 }
                 var top = $(document).scrollTop();
                 var speed = 300;
-                if (top >= _this.firstScreenHeight) {
+                if (top >= _this.firstScreenHeight + _this.headerHeight) {
                     $('.t-page-nav').addClass('on').stop().animate(500);
                 } else {
                     $('.t-page-nav').removeClass('on').stop().animate(speed);
