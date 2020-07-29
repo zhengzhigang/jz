068af957c17b2e442358af1a0af54ea375ce9d92
郑智刚 <zhengzhigang@igengmei.com>
2020-07-29 18:59:50 +0800

    添加swiper

---
+++ ES205GC-P/RJ-js/main.js
@@ -0,0 +1,26 @@
+$(function() {
+    var main = {
+        initMain: function() {
+            this.initSwiper();
+            console.log('***', Swiper)
+        },
+        initSwiper: function() {
+            var mySwiper = new Swiper('.swiper-container',{
+                loop: true,
+                on: {
+                    slideChangeTransitionStart:function(e) {
+                        var i = this.activeIndex;
+                        $('.section-1 .thumb-item').eq(i).addClass('active').siblings().removeClass('active');
+                    }
+                }
+                //其他设置
+            });
+            console.log(mySwiper)
+            $('.section-1 .thumb-item').on('click', function() {
+                var i = $(this).index();
+                mySwiper.slideTo(i)
+            })
+        }
+    }
+    main.initMain();
+});
