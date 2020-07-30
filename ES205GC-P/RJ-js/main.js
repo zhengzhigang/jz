8e66e8ea8a1b94bd6e1eb22384e0b4779176aca7
郑智刚 <zhengzhigang@igengmei.com>
2020-07-30 14:36:52 +0800

    fix bug

--- ES205GC-P/RJ-js/main.js
+++ ES205GC-P/RJ-js/main.js
@@ -4,14 +4,17 @@ $(function() {
             this.initSwiper();
         },
         initSwiper: function() {
+            console.log('&&&&')
             var mySwiper = new Swiper('.swiper-container',{
                 loop: true,
+                initialSlide: 0,
                 autoplay: {
-                    delay: 2000
+                    delay: 2000,
+                    disableOnInteraction: false
                 },
                 on: {
-                    slideChangeTransitionStart:function(e) {
-                        var i = this.activeIndex;
+                    slideChangeTransitionStart:function() {
+                        var i = this.realIndex;
                         $('.section-1 .thumb-item').eq(i).addClass('active').siblings().removeClass('active');
                     }
                 }
@@ -20,7 +23,7 @@ $(function() {
             console.log(mySwiper)
             $('.section-1 .thumb-item').on('click', function() {
                 var i = $(this).index();
-                mySwiper.slideTo(i)
+                mySwiper.slideToLoop(i)
             })
         }
     }
