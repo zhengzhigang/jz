5eb139cd5e1217908ed9fc5812ab9dc9b8f51595
郑智刚 <zhengzhigang@igengmei.com>
2020-08-01 18:11:05 +0800

    EG210G-P

---
+++ EG210G-P/RJ-js/main.js
@@ -0,0 +1,29 @@
+$(function() {
+    var main = {
+        initMain: function() {
+            this.initSwiper();
+        },
+        initSwiper: function() {
+            var mySwiper = new Swiper('.swiper-container',{
+                loop: true,
+                initialSlide: 0,
+                autoplay: {
+                    delay: 2000,
+                    disableOnInteraction: false
+                },
+                on: {
+                    slideChangeTransitionStart:function() {
+                        var i = this.realIndex;
+                        $('.section-1 .thumb-item').eq(i).addClass('active').siblings().removeClass('active');
+                    }
+                }
+            });
+            console.log(mySwiper)
+            $('.section-1 .thumb-item').on('click', function() {
+                var i = $(this).index();
+                mySwiper.slideToLoop(i)
+            })
+        }
+    }
+    main.initMain();
+});
--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "ES05G/RJ-css"
-sass_dir = "es-05-scss"
+css_dir = "EG210G-P/RJ-css"
+sass_dir = "eg-scss"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
