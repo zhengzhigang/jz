c834d37d8cb97dfc788b4e5a78cc10b6bd8cafd8
郑智刚 <zhengzhigang@igengmei.com>
2020-08-05 13:51:42 +0800

    RG-EG105G

--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "complete/NBS3200/RJ-css"
-sass_dir = "complete/nb-scss"
+css_dir = "RG-EG105G/RJ-css"
+sass_dir = "g-scss"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
