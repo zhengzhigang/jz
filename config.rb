655856f8fe0f9a3dcba2b0d759bad24323bba9ba
郑智刚 <zhengzhigang@igengmei.com>
2020-08-06 13:43:30 +0800

    RG-NBS5100-48GT4SFP

--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "RG-EG105G/RJ-css"
-sass_dir = "g-scss"
+css_dir = "RG-NBS5100-48GT4SFP/RJ-css"
+sass_dir = "rg-nb-scss"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
