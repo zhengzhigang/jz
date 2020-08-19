1e64a9c7125af7dc07b2a1dfd754e2dd7d536a19
郑智刚 <zhengzhigang@igengmei.com>
2020-08-19 14:01:02 +0800

    zzg-cn-20200819

--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "complete/NBS3200/RJ-css"
-sass_dir = "complete/nb-scss"
+css_dir = "zzg-cn-20200819/RJ-css"
+sass_dir = "zzg-cn-20200819-scss"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
