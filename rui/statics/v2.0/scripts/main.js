e9df753ef8ad2f3a44b5c5199906348b0407f985
郑智刚 <zhengzhigang@igengmei.com>
2020-09-01 15:50:56 +0800

    瑞易网络

---
+++ rui/statics/v2.0/scripts/main.js
@@ -0,0 +1,14 @@
+$(function() {
+	
+	
+	var swiper = new Swiper('.swiper-container',{
+        autoplay:true,
+        loop:true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+      },
+
+    });
+
+});
