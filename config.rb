a8171fa2686b8b645ff4b0fd9dfddea6be67446e
郑智刚 <zhengzhigang@igengmei.com>
2020-09-24 20:23:47 +0800

    RGEW1200G

--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "smart-PW/RJ-css"
-sass_dir = "smart-scss"
+css_dir = "RGEW1200G/RJ-css"
+sass_dir = "rg-scss"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
