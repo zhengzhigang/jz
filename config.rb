2d8bbae5c2a923c205afd239604e80fc308489d1
郑智刚 <zhengzhigang@igengmei.com>
2020-09-24 09:51:20 +0800

    smart-PW

--- config.rb
+++ config.rb
@@ -4,8 +4,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "vi/RJ-css"
-sass_dir = "vi-scss"
+css_dir = "smart-PW/RJ-css"
+sass_dir = "smart-scss"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
