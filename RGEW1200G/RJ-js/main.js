aaf0cef342dc3db41b54c336712590a6528b98ae
郑智刚 <zhengzhigang@igengmei.com>
2020-09-27 21:48:42 +0800

    style

--- RGEW1200G/RJ-js/main.js
+++ RGEW1200G/RJ-js/main.js
@@ -9,15 +9,13 @@ $(function() {
             var mySwiper = new Swiper('.swiper-container',{
                 loop: true,
                 initialSlide: 0,
+                navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+                },
                 autoplay: {
                     delay: 2000,
                     disableOnInteraction: false
-                },
-                on: {
-                    slideChangeTransitionStart:function() {
-                        var i = this.realIndex;
-                        $('.section-1 .thumb-item').eq(i).addClass('active').siblings().removeClass('active');
-                    }
                 }
             });
             $('.section-1 .thumb-item').on('click', function() {
