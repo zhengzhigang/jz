cdaf0bd09ef6ba598856e07e20eda05508b7e3f4
郑智刚 <zhengzhigang@igengmei.com>
2020-10-19 19:56:20 +0800

    index.shtml

--- biwu/RJ-js/main.js
+++ biwu/RJ-js/main.js
@@ -31,11 +31,32 @@ $(function() {
             });
             peopleScrollContent
 
-            new Swiper('#scrollContent', options)
+            var swiper = new Swiper('#scrollContent', options)
 
             new Swiper('#peopleScrollContent', options);
 
             new Swiper('#rankScrollContent', options);
+
+            var startScroll, touchStart, touchCurrent;
+            swiper.slides.on('touchstart', function (e) {
+                startScroll = this.scrollTop;
+                touchStart = e.targetTouches[0].pageY;
+            }, true);
+            swiper.slides.on('touchmove', function (e) {
+                touchCurrent = e.targetTouches[0].pageY;
+                var touchesDiff = touchCurrent - touchStart;
+                var slide = this;
+                var onlyScrolling = 
+                        ( slide.scrollHeight > slide.offsetHeight ) && //allow only when slide is scrollable
+                        (
+                            ( touchesDiff < 0 && startScroll === 0 ) || //start from top edge to scroll bottom
+                            ( touchesDiff > 0 && startScroll === ( slide.scrollHeight - slide.offsetHeight ) ) || //start from bottom edge to scroll top
+                            ( startScroll > 0 && startScroll < ( slide.scrollHeight - slide.offsetHeight ) ) //start from the middle
+                        );
+                if (onlyScrolling) {
+                    e.stopPropagation();
+                }
+            }, true);
         },
         initOnNav: function() {
             new Scrollspy({
