9febbfe882cd8ce294b5eac0173549c4bb773f59
郑智刚 <zhengzhigang@igengmei.com>
2020-12-17 11:29:09 +0800

    delete

--- complete/c-rui/statics/js/ruijiery/d2.js
+++
@@ -1,8 +0,0 @@
-
-$(window).load(function() {
-        
-    $(".diamondswrap").diamonds({
-        size: 160, // Size of the squares
-        gap: 7// Pixels between squares
-    });
-});
--- complete/c-rui/statics/v3.0/scripts/footer.js
+++
@@ -1,9 +0,0 @@
-(function ($, window, document) {
-  $(window).load(function () {
-    if ($(window).innerWidth() < 768) {
-      $('.ry-footer-item h4').on('click', function () {
-        $(this).toggleClass('on').next('ul').slideToggle()
-      })
-    }
-  })
-})(jQuery, window, document)
