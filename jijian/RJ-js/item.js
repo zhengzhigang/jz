ff70ef6307fd43a79c9093bc3e3a0de1eaa2082b
zhengzhigang <813726742@qq.com>
2021-08-08 21:18:41 +0800

    添加链接

--- jijian/RJ-js/item.js
+++ jijian/RJ-js/item.js
@@ -1,15 +1,17 @@
 $(function () {
-  var width = document.body.clientWidth
-  var swiper = new Swiper('.page5-swiper', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-    centeredSlides: true,
-    autoplay: true,
-    pagination: {
-      el: '.swiper-pagination'
-    },
-    autoHeight: true
+  $('#showDialog1').click(function() {
+    $('#dialog1').show()
+  })
+
+  $('#showDialog2').click(function() {
+    $('#dialog2').show()
+  })
+
+  $('#cloasDialog1').click(function() {
+    $('#dialog1').hide()
+  })
+
+  $('#cloasDialog2').click(function() {
+    $('#dialog2').hide()
   })
 })
