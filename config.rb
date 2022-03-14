fcf92618f667e67b41ed9c0252f2b637e3414a1d
zhengzhigang <813726742@qq.com>
2022-03-14 23:51:03 +0800

    japan1修改

--- config.rb
+++ config.rb
@@ -7,8 +7,8 @@ http_path = "/"
 # css_dir = "gaomixing/statics/v4.0/styles"
 # sass_dir = "gaomixing-scss"
 
-css_dir = "japan3/RJ-css/"
-sass_dir = "japan3-scss"
+css_dir = "japan1/RJ-css/"
+sass_dir = "japan1-scss"
 
 # css_dir = "poster/css"
 # sass_dir = "poster-scss"
